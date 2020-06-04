6802c6443f70519f42886fa35b3d348c30661538
tqtrunghnvn <tranquangtrunghnvn@gmail.com>
2020-06-04 22:50:52 +0900

    update

--- train.sh
+++ train.sh
@@ -1,1 +1,2 @@
-python train.py --dataset cifar10 --num-labeled 4000 --arch wideresnet --batch-size 64 --lr 0.03 --out cifar10@4000
+#python train.py --dataset cifar10 --num-labeled 4000 --arch wideresnet --batch-size 64 --lr 0.03 --out cifar10@4000
+python train.py --dataset cifar10 --num-labeled 4000 --arch wideresnet --batch-size 64 --lr 0.03 --out cifar10_trip@4000
